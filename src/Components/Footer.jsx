b5fcaae28629c0fdf656edf3b19c0d06acbec60d
prejin2310 <prejinproffical@gmail.com>
2023-05-24 08:30:53 +0530

    about page redirect fixed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style.css";
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>DoingFreelance © 2023</p>
+      </footer>
+  )
+}
+
+export default Footer
