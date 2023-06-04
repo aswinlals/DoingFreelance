bc8bd1bd196ae74c6325f314c01ad2c5c5ac4046
aswinlals <aswinlalmuthuvila@gmail.com>
2023-06-04 10:56:32 +0530

    Updated Navbar with a working Connect Button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../style.css";
 import logo from "../assets/doingfreelance.png";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
+const Navbar = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
 
-const navbar = () => {
+  async function connectWallet() {
+    if (walletAddress) {
+      // Disconnect wallet
+      setWalletAddress(null);
+    } else if (typeof window.ethereum !== "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const address = accounts[0];
+        setWalletAddress(address);
+      } catch (error) {
+        console.log("Error connecting wallet:", error);
+      }
+    } else {
+      console.log("No MetaMask!");
+    }
+  }
+
   return (
     <div>
       <header>
@@ -13,19 +33,39 @@ const navbar = () => {
         </a>
 
         <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/browse">Browse Freelancers</Link></li>
-          <li><Link to="/postjob">Register as Freelancer</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/how">How it works</Link></li>
-        </ul>
-      </nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/browse">Browse Freelancers</Link>
+            </li>
+            <li>
+              <Link to="/postjob">Register as Freelancer</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/how">How it works</Link>
+            </li>
+          </ul>
+        </nav>
 
-        <button id="connect-wallet">Connect Wallet</button>
+        <button
+          id="connect-wallet"
+          onClick={connectWallet}
+          title={walletAddress ? `Connected: ${walletAddress}` : "Connect Wallet"}
+        >
+          {walletAddress ? "Disconnect" : "Connect"}
+        </button>
+
+        {/* {walletAddress && (
+          <p>Connected wallet address: {walletAddress}</p>
+        )} */}
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
